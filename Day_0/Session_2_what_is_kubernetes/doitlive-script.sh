50abc63f95e1132cda8e22a5ec0a2e5919397e4e
wobble-socket <271287259+wobble-socket@users.noreply.github.com>
2026-04-07 15:48:20 -0500

    doitlive formatting, page autorefresh

--- Day_0/Session_2_what_is_kubernetes/doitlive-script.sh
+++ Day_0/Session_2_what_is_kubernetes/doitlive-script.sh
@@ -5,45 +5,53 @@
 #doitlive prompt: $
 #doitlive alias: cat='batcat --paging=never'
 
-#1.1. Execution: Docker is easy!
+#Docker is easy!
 docker run hello-world
 
 #
-#1.2. Execution: Though you do need to remember some flags...
+#Though you do need to remember some flags...
 docker run --rm -it alpine sh -c "echo -e 'not so bad I guess...'"
 
 #
-#1.3. Execution: Let's run our website. This gets complicated.
+#Let's run our website.
 ls obscurity/
-docker run --rm --name obscurity-demo -e CREATED_VIA="Docker Run" -v $(pwd)/obscurity:/usr/share/caddy:ro -p 8080:80 caddy:alpine caddy file-server --root /usr/share/caddy --templates --access-log
-clear
 
 #
-#2.1. Declaration: Compose makes the complexity easier.
+#Uh oh...this gets complicated.
+docker run --rm --name obscurity-demo -e CREATED_VIA="Docker Run" -v $(pwd)/obscurity:/usr/share/caddy:ro -p 8080:80 caddy:alpine caddy file-server --root /usr/share/caddy --templates --access-log
+#
+#Great! Now do that again 100 times...
+clear
+
+
+#Compose makes the complexity easier.
 cat compose.yaml
 
 #
-#2.2. Declaration: Now that we've declared, execution is simple.
+#Now that we've declared, execution is simple.
 docker compose up
 docker compose down
+#
+#Great! Now do that again 100 times...
 clear
 
-#
-#3.1. Scale: Let's start a local cluster
+
+#Let's start a local cluster
 cat kind-config.yaml
 kind create cluster --name obscurity-cluster --config kind-config.yaml
 clear
 
-#
-#3.1. Scale: The complexity of scale makes our declarations much larger...
+
+#The complexity of scale makes our declarations much larger...
 cat manifest.yaml
 
 #
-#3.1. Scale: But executing at scale is made simple.
+#But executing at scale is made simple.
 kubectl apply -f manifest.yaml
 kubectl get pods -n obscurity-demo -w
+#
 kubectl logs -f obscurity-pod -n obscurity-demo
 clear
 
-#
+
 #End of demo!
